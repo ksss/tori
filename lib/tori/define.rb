4e5b696f04f19eb68a4f06bebf180ca4c4f6b1f2
ksss <co000ri@gmail.com>
2015-01-26 17:09:09 +0900

    Rename ivar to simple name.

--- lib/tori/define.rb
+++ lib/tori/define.rb
@@ -1,16 +1,16 @@
 module Tori
   module Define
     def tori(name)
-      name_file_ivar = "@#{name}_file".to_sym
+      name_ivar = "@#{name}".to_sym
 
       define_method(name) do
-        ivar = instance_variable_get name_file_ivar
-        instance_variable_set name_file_ivar, ivar || File.new(self)
+        ivar = instance_variable_get name_ivar
+        instance_variable_set name_ivar, ivar || File.new(self)
       end
 
       define_method("#{name}=") do |uploader|
         file = File.new(self, uploader)
-        instance_variable_set name_file_ivar, file
+        instance_variable_set name_ivar, file
       end
     end
   end
