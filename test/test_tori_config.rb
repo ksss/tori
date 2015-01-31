6b80009f8e521aec0ad1e08be5d0ff691770e170
ksss <co000ri@gmail.com>
2015-01-31 09:32:46 +0900

    Refactor test

--- test/test_tori_config.rb
+++ test/test_tori_config.rb
@@ -4,9 +4,5 @@ class TestToriConfig < Test::Unit::TestCase
   test "#initialize" do
     i = Tori::Config.new
     assert_instance_of Tori::Config, i
-    assert_respond_to i, :backend
-    assert_respond_to i, :backend=
-    assert_respond_to i, :filename_callback
-    assert_respond_to i, :filename_callback=
   end
 end
--- test/test_tori_file.rb
+++ test/test_tori_file.rb
@@ -14,9 +14,9 @@ class TestToriDefine < Test::Unit::TestCase
     FileUtils.rm_rf("test/tmp")
   end
 
-  class Dammy
+  class From
     def path
-      "path"
+      __FILE__
     end
   end
 
@@ -35,6 +35,12 @@ class TestToriDefine < Test::Unit::TestCase
 
   test "#copy?" do
     assert { false == Tori::File.new(__FILE__, nil).copy? }
-    assert { true == Tori::File.new(__FILE__, Dammy.new).copy? }
+    assert { true == Tori::File.new(__FILE__, From.new).copy? }
+  end
+
+  test "copy" do
+    assert { false == File.exist?("test/tmp/copy") }
+    Tori::File.new("copy", From.new).copy
+    assert { true == File.exist?("test/tmp/copy") }
   end
 end
