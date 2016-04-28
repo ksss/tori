a1c8acbb020cf66d2dfaf0eeaa758fdcd1af90b5
ksss <co000ri@gmail.com>
2016-04-28 19:04:06 +0900

    Move to set only

--- lib/tori/config.rb
+++ lib/tori/config.rb
@@ -7,9 +7,9 @@ module Tori
     end
 
     def filename_callback(&block)
-      warn "DEPRECATED: `#{__method__}' is deprecated method."
-      warn "Please use `tori` method block style in model like `tori :name, { |model| model.id }`."
       if block_given?
+        warn "DEPRECATED: `#{__method__}' is deprecated method."
+        warn "Please use `tori` method block style in model like `tori :name, { |model| model.id }`."
         @filename_callback = block
       else
         @filename_callback
